52310d67ab3b4b8b78489784074fdb14bb49d63d
rafael <rafael.naranjo@qtagtech.com>
2014-12-02 16:24:00 -0500

    instrucciones para eliminar usuario

--- grails-app/controllers/com/freeAppListing/users/UserController.groovy
+++ grails-app/controllers/com/freeAppListing/users/UserController.groovy
@@ -10,6 +10,16 @@ class UserController {
 
     @Transactional
     def delete(){
+        /**
+         * Solo se elimina usuario tipo user
+         */
 
+        // Recibir id o ids de USER a eliminar
+        // Validar que existan y que no sea usuario manager
+        // se valida por company si tiene aplicaciones y campañas
+        // Se elimina las app y campañas que tenga el usuario
+        // se eliminar la relacion de role y usuario
+        // Se elimina el usuario y la compañia
+        // Se responde si fue exitoso o no la eliminacion del usuario
     }
 }
