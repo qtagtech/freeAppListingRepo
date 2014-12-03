4d23695d10f21757d9ca74569c36401993763d9d
rafael <rafael.naranjo@qtagtech.com>
2014-12-03 18:45:43 -0500

    eliminar aplicacion con sus conexiones

--- grails-app/controllers/com/freeAppListing/application/ApplicationController.groovy
+++ grails-app/controllers/com/freeAppListing/application/ApplicationController.groovy
@@ -4,6 +4,7 @@ import com.freeAppListing.company.Company
 import com.freeAppListing.link.Link
 import com.freeAppListing.platform.Platforms
 import com.mongodb.BasicDBObject
+import com.mongodb.DBCollection
 import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 import grails.transaction.Transactional
@@ -84,21 +85,58 @@ class ApplicationController {
 
     @Transactional
     def delete(){
-
+        def db = mongo.getDB(grailsApplication.config.com.freeAppListing.database)
         def respuesta
 
+        // validate that id of app is not null or empty
         if( StringUtils.isEmpty(params.id.toString())){
             throw new Exception("Id is null")
+            response.setStatus(400)
+            respuesta = [status:0]
+            render respuesta as JSON
+            return
         }
 
+        // get the app for id
         def application = Application.get(params.id)
+        if(application==null){
+            throw  new Exception("The application don't exist.")
+            response.setStatus(400)
+            respuesta = [status:0]
+            render respuesta as JSON
+            return
+        }
 
+        def resultOne
+        def resultTwo
+        def resultTre
+        // valiate if have campaigns
+        DBCollection campaing = db.getCollection("campaign");
+        BasicDBObject qCampaing = new BasicDBObject().append("application._id",application.id)
+        def haveCampaigns = campaing.count(qCampaing)
+        if(haveCampaigns>0){
+            resultOne = campaing.remove(qCampaing)
+        }
+
+        // valiate if have clicks
+        BasicDBObject qClicks = new BasicDBObject().append("appId",application.id)
+        def haveClicks = db.conversion.count(qClicks)
+        if(haveClicks>0){
+            resultTwo = db.conversion.remove(qClicks)
+        }
+        // valiate if have installs
+        BasicDBObject qInstall = new BasicDBObject().append("appId",application.id)
+        def haveInstalls = db.serve.count(qInstall)
+        if(haveInstalls>0){
+            resultTre = db.serve.remove(qInstall)
+        }
+
+        // if diferent to null delete the app
         if(application.nombre != null || application.nombre != ""){
             application.delete()
         }
 
         respuesta = [status:1]
         render respuesta as JSON
-
     }
 }
